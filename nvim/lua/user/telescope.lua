7cd19c9fa99bee763374fcd2d7cc17f78636da18
James Middleton <james.ben.middleton@gmail.com>
2024-04-26 15:49:25 +0100

    Added ability to toggle ignored files and provide glob file search path to live_grep.

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -72,21 +72,55 @@ vim.cmd([[autocmd User TelescopePreviewerLoaded setlocal wrap]])
 local keymap = vim.keymap.set
 local opts = {noremap = true, silent = true}
 
--- grep find text across working directory
-keymap("n", "<leader>g", ":Telescope live_grep<CR>", opts)
-
--- open files in working directory
-keymap("n", "<leader>o", ":Telescope find_files<CR>", opts)
-
 -- view open buffers
 keymap("n", "<leader>f", ":Telescope buffers<CR>", opts) -- mnemonic < ease of use
 
 -- view git modified files
 keymap("n", "<leader>c", ":Telescope git_status<CR>", opts) 
 
--- find files ignored by git
-vim.api.nvim_create_user_command("FindIgnored", function()
-    require "telescope.builtin".find_files{no_ignore=true, hidden=true}
+-- toggle whether finders should look in ignored / hidden files
+vim.g.telescope_ignore_toggle = false
+vim.api.nvim_create_user_command("ToggleIgnore", function()
+    if vim.g.telescope_ignore_toggle == true then
+        print("Ignores are now excluded")
+    else
+        print("Ignores are now included")
+    end
+    vim.g.telescope_ignore_toggle = not vim.g.telescope_ignore_toggle
 end, {}) 
 
+-- grep find text across working directory
+keymap("n", "<leader>g", function()
+    local fwd_args = {}
+    if vim.g.telescope_ignore_toggle == true then
+        fwd_args = {"-uuu"}
+    end
+    require "telescope.builtin".live_grep{
+        additional_args = fwd_args
+    }
+end, opts)
 
+-- open files in working directory
+keymap("n", "<leader>o", function()
+    require "telescope.builtin".find_files{
+        no_ignore=vim.g.telescope_ignore_toggle,
+        hidden=vim.g.telescope_ignore_toggle
+    }
+end, opts)
+
+-- live_grep the provided glob input
+vim.api.nvim_create_user_command("Find", function(args)
+    local fwd_args = {}
+    if vim.g.telescope_ignore_toggle == true then
+        fwd_args = {"-uuu"}
+    end
+
+    glob = args['args']
+    if glob == "" then
+        glob = "**/*"
+    end
+    require "telescope.builtin".live_grep{
+        glob_pattern=glob,
+        additional_args=fwd_args
+    }
+end, { nargs='*' })
